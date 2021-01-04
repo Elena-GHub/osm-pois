2043b7d8ca1276ca40ff357d1fd115ba11784b0c
Elena-GHub <cabjelen@yahoo.es>
2021-01-04 10:50:38 +0100

    added Tag model and Poi-Tag ManyToMany relationship

--- upoi/poi/models/poi.py
+++ upoi/poi/models/poi.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .tag import Tag
 # from django.contrib.gis.db import models
 
 
@@ -11,6 +12,7 @@ class Poi(models.Model):
     # coordinates = models.PointField()
     coordinates = models.CharField(max_length=100)
     social_media = models.CharField(max_length=300)
+    tags = models.ManyToManyField(Tag)
 
 
     def __str__(self):
